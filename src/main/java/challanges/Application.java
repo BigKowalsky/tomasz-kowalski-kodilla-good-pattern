2bf83f14dceff8e2affecdf55e3a3b60c33f86d8
Tomasz Kowalski <tomek.kowalski.89@gmail.com>
2022-09-23 22:14:33 +0200

    MovieStore

--- src/main/java/challanges/Application.java
+++ src/main/java/challanges/Application.java
@@ -1,2 +1,14 @@
-package challanges;public class Application {
+package challanges;
+
+import java.lang.*;
+import java.util.stream.Collectors;
+
+public class Application {
+    public static void main(String args[]){
+        MovieStore movieStore = new MovieStore();
+        String movies = movieStore.getMovies().entrySet().stream()
+                .flatMap(m -> m.getValue().stream())
+                .collect(Collectors.joining(" ! ","<< "," >>"));
+        System.out.println(movies);
+    }
 }
